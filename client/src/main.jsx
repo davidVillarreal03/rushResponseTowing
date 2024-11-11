2b54962190adc0787b203d2cd21d1293c9a50a6b
David Villarreal <djmarch92014@gmail.com>
2024-11-11 16:31:12 -0600

    Setting up the folders and file for client side

---
+++ client/src/main.jsx
@@ -0,0 +1,45 @@
+import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+
+import App from './App'
+import ErrorPage from '.pages/ErrorPage';
+import Home from './pages/Home';
+import Login from './pages/Login';
+import About from './pages/About';
+import Services from './pages/Services';
+import Contact from './pages/Contact';
+
+const router = createBrowserRouter([
+  {
+    path: '/', 
+    element: <App />, 
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        index: true, 
+        element: <Home />,
+
+      },
+      {
+        path: '/login', 
+        element: <Login />,
+      },
+      {
+        path: '/about', 
+        element: <About />,
+      },
+      {
+        path: '/services', 
+        element: <Services />,
+      },
+      {
+        path: '/contact', 
+        element: <Contact />,
+      },
+    ]
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <RouterProvider router={router} />
+)
