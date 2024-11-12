e028babb3d5d858f63e5d8c46eacf69587a3dd61
David Villarreal <djmarch92014@gmail.com>
2024-11-11 18:38:31 -0600

    working on nav bar

--- client/src/components/Navigation/Navigation.jsx
+++ client/src/components/Navigation/Navigation.jsx
@@ -5,41 +5,44 @@ function NavTabs() {
     const currentPage = useLocation().pathname;
 
     return (
-        <ul className="nav nav-tabs">
-            <li className="nav-item">
-                <Link
-                    to="/"
-                    className={currentPage === "/" ? "nav-link active" : "nav-link"}
-                >
-                    Home
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/About"
-                    className={currentPage === "/About" ? "nav-link active" : "nav-link"}
-                >
-                    About
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/Services"
-                    className={currentPage === "/Services" ? "nav-link active" : "nav-link"}
-                >
-                    Services
-                </Link>
-            </li>
-            <li className="nav-item">
-                <Link
-                    to="/Contact"
-                    className={currentPage === "/Contact" ? "nav-link active" : "nav-link"}
-                >
-                    Contact
-                </Link>
-            </li>
-        </ul>
-    );
+        <div className="container">
+
+            <ul className="nav nav-tabs">
+                <li className="nav-item">
+                    <Link
+                        to="/"
+                        className={currentPage === "/" ? "nav-link active" : "nav-link"}
+                    >
+                        Home
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link
+                        to="/About"
+                        className={currentPage === "/About" ? "nav-link active" : "nav-link"}
+                    >
+                        About
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link
+                        to="/Services"
+                        className={currentPage === "/Services" ? "nav-link active" : "nav-link"}
+                    >
+                        Services
+                    </Link>
+                </li>
+                <li className="nav-item">
+                    <Link
+                        to="/Contact"
+                        className={currentPage === "/Contact" ? "nav-link active" : "nav-link"}
+                    >
+                        Contact
+                    </Link>
+                </li>
+            </ul>
+        </div>
+        );
 }
 
 export default NavTabs;
