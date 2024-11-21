e573c15288e40078549be798cc584375a7c7e890
David Villarreal <djmarch92014@gmail.com>
2024-11-21 15:43:27 -0600

    added the proper phone number instead of test

--- src/components/Phone/phone.jsx
+++ src/components/Phone/phone.jsx
@@ -3,7 +3,7 @@ import './phone.css';
 function Phone() {
     return (
         <div className='phone'>
-            <a href='tel:+210-274-2785' title='Give us a call'>
+            <a href='tel:+210-954-9547' title='Give us a call'>
                 <i className="fa-solid fa-phone" title='Give us a call'></i>
             </a>
         </div>
