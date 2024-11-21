d3f9ea044d95df03048466bc8c963228978ee5b6
David Villarreal <djmarch92014@gmail.com>
2024-11-21 14:14:45 -0600

    added svg and promo banner

--- client/src/components/Header/Header.jsx
+++ client/src/components/Header/Header.jsx
@@ -3,7 +3,7 @@ import './Header.css';
 function Header() {
     return (
         <div className="img header">
-            <p id='logo'>RRT</p>
+            <img id='logo'src='../../rrt.svg' alt='logo for the company Rush Response Towing.'/>
             <p>Rushing to hook you up.</p>
         </div>
     )
--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,13 +1,20 @@
 import './Home.css'
-// const image = require('../../../public/images/professionaltowtruck.jpg');
 export default function Home() {
     return (
-        <div>
-            <h1 className='home'>FAST, RELIABLE TOWING SERVICES - ANYTIME, ANYWHERE!</h1>
-            
-            <div title='Give us a call' className='phone'>
-                <i className="fa-solid fa-phone" title='Give us a call'></i>
+        <div className='home'>
+            <div className='promo'>
+                <h2>Get a Free Tow!</h2>
+                <p>Your next tow can be free when you repair your vehicle
+                    at you closest <strong>Midas</strong>!<br></br>   
+                     <em>
+                     Only at participating locations.
+                    </em>
+                </p>
             </div>
+            <img src='../../images/professiontowtruck.jpg' alt='Tow truck in action of taking tow vehicles away from accident.'/>
+            <h1>FAST, RELIABLE TOWING SERVICES - ANYTIME, ANYWHERE!</h1>
+            <p>24/7 Roadside Assistance to Get You Back on Track.</p>
+            <p>dhsdlfasd</p>
         </div>
     )
 }
